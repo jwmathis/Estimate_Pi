f7c54eab9061892f9b321529c20c28b2d3fcdec9
jwmathis <jwmathis7@gmail.com>
2025-08-27 20:31:27 -0400

    added flask to py file

--- estimate_pi.py
+++ estimate_pi.py
@@ -5,7 +5,7 @@ import math
 # Initialize Flask Application
 app = Flask(__name__)
 
-"""Core Functions"""
+# Core Functions to estimate Pi
 def count_decimal_places(number):
     """
     Counts the number of decimal places in a number.
@@ -40,38 +40,39 @@ def estimate_pi(num_points):
             pointsInCircle += 1
 
     pi = (4 * pointsInCircle) / totalPoints
+    formatted_pi = round(pi, 3)
     return pi
 
 # Define the route for the home page
 @app.route('/', methods=['GET', 'POST'])
+def index():
+    """
+    Handles requests to the /index route.
+    On GET request, it shows the form.
+    On POST request, it calculates Pi and displays results
+    """
 
-def main():
-    """
-    Main function
-    :return:
-    """
-    while True:
+    pi_estimate = 0
+    num_points = 0
+
+    # Check if form was submitted via a POST
+    if request.method == 'POST':
         try:
-            num_decimal_places = int(input("Enter the number of decimal places you would like to use: "))
-            if num_decimal_places == 0:
-                break
-            elif num_decimal_places >= 5:
-                num_points = 1000000000
-            else:
-                num_points = 10**(num_decimal_places + 2)
+            # Get number of points from the HTML form
+            num_points_str = request.form['num_points']
+            num_points = int(num_points_str)
 
-            print(f"\Running Monte Carlo simulation for {num_points} points.")
+            # Run simulation and get Pi estimation
+            if num_points > 0:
+                pi_estimate = estimate_pi(num_points)
 
-            pi_approximation = estimate_pi(num_points)
+        except (ValueError, KeyError):
+            # Error handling when input is not a valid number
+            pi_estimate = "Invalid input. Enter a positive whole number."
+            num_points = "N/A"
 
-            formatted_pi = round(pi_apporximation, num_decimal_places)
-
-            print(f"\Pi calculated is approximately {pi_approximation}")
-            print(f"\Pi is approximately {formatted_pi}")
-            break
-
-        except ValueError:
-            print("Please enter a number greater than 0")
+    # Render HTML template using results
+    return render_template('index.html', pi_estimate=pi_estimate, num_points=num_points)
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
